b4e5172c1d8577abe0e3c5d32a7b30c1a9a37153
Maicon R. Silva <maiconrods@gmail.com>
2020-04-20 15:49:40 -0300

    starting the development of the problem

--- main.php
+++ main.php
@@ -4,6 +4,15 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 use Reweb\Job\Backend;
 
-$exemplo = new Backend\Exemplo;
+$accountOne = new Backend\CashMachine(10000, 1);
+$accountTwo = new Backend\CashMachine(3000, 2);
 
-echo $exemplo->exemplo();
+$accountTwo -> transfer($accountOne -> transfer(2000, 1), 2);
+
+$accountOne -> displayBalance();
+$accountTwo -> displayBalance();
+
+$accountTwo -> withdraw(1000);
+$accountTwo -> displayBalance();
+
+
---
+++ src/CashMachine.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace Reweb\Job\Backend;
+
+class CashMachine {
+  public function __construct($balance, $accountType) {
+    $this -> balance = $balance;
+    $this -> accountType = $accountType;
+  }
+  
+  public function deposit($value) {
+    if($value < 2) {
+      return $this -> errorMessage(3);
+    }
+    return $this -> balance += $value;
+  }
+
+  public function withdraw($value) {
+    switch($this -> accountType) {
+      case 1:
+        $withdrawTax = 2.50;
+        if($value > $this -> balance) {
+          return $this -> errorMessage(1);
+        } 
+        if($value > 600) {
+          return $this -> errorMessage(2);
+        }
+        return $this -> balance -= $value + $withdrawTax;
+      break;
+      case 2:
+        $withdrawTax = 0.80;
+        if($value > $this -> balance) {
+          return $this -> errorMessage(1);
+        } 
+        if($value > 1000) {
+          return $this -> errorMessage(2);
+        }
+        return $this -> balance -= $value + $withdrawTax;
+      break;
+    }
+  }
+
+  public function transfer($value, $operationType) {
+    switch($operationType) {
+      case 1:
+        if($value > $this -> balance) {
+          return $this -> errorMessage(1);
+        }
+        $this -> balance -= $value;
+        return $value;
+      break;
+      case 2:
+        return $this -> balance += $value;
+      break;
+    }
+  }
+
+  public function displayBalance() {
+    echo "\nB$ " . $this -> balance;
+  }
+
+  public function errorMessage($error) {
+    switch($error) {
+      case 1:
+        echo "\nerror: Insuficient funds.";
+      break;
+      case 2:
+        echo "\nerror: Operation value exceeded.";
+      break;
+      case 3:
+        echo "\nerror: Minimun deposit value is B$ 2.00";
+      break;
+    }
+  }
+}
